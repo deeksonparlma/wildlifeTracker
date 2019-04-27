faaa3d9c41c0ef29684b13313d03aaf2c82c799e
deeksonparlma <dicksonparlma@gmail.com>
2019-04-27 11:24:40 +0300

    added the age property

--- src/main/java/Report.java
+++ src/main/java/Report.java
@@ -5,12 +5,13 @@ public class Report {
     private String name;
     private String health;
     private String age;
-    public Report(String rangername,String category, String zone,String name,String health){
+    public Report(String rangername,String category, String zone,String name,String health,String age){
       this.rangerName = rangername;
       this.category = category;
       this.zone = zone;
       this.rangerName = rangername;
       this.name = name;
       this.health =health;
+      this.age = age;
     }
 }
