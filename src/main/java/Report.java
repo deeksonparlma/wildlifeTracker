113bfa61cb29cb2a39718689d68b5ae06d110ece
deeksonparlma <dicksonparlma@gmail.com>
2019-04-27 11:34:54 +0300

    added the  delete,save functionality

--- src/main/java/Report.java
+++ src/main/java/Report.java
@@ -1,3 +1,5 @@
+import org.sql2o.Connection;
+
 public class Report {
     private String rangerName;
     private String category;
@@ -5,6 +7,7 @@ public class Report {
     private String name;
     private String health;
     private String age;
+    private int id;
     public Report(String rangername,String category, String zone,String name,String health,String age){
       this.rangerName = rangername;
       this.category = category;
@@ -37,5 +40,43 @@ public class Report {
 
     public String getZone() {
         return zone;
+    }
+
+    public int getId() {
+        return id;
+    }
+    //functionality//
+
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO sighting (rangername, category, zone, name, health, age, categoryid) VALUES (:rangername, :category, :zone, :name , :health , :age , :categoryid)";
+            this.id= (int) con.createQuery(sql, true)
+                    .addParameter("rangername", this.rangerName)
+                    .addParameter("category", this.category)
+                    .addParameter("zone", this.zone)
+                    .addParameter("name", this.name)
+                    .addParameter("health", this.health)
+                    .addParameter("age", this.age)
+                    .executeUpdate()
+                    .getKey();
+        }
+
+
+    }
+    public void delete() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM sighting WHERE id = :id;";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+    public static Report find(int id) {
+        try(Connection con =   DB.sql2o.open()) {
+            String sql = "SELECT * FROM sighting where id = :id;";
+            return con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Report.class);
+        }
     }
 }
