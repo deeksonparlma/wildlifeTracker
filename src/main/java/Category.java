cee3f2e72a45eb29b13de498cad1eaded9c6c4fe
deeksonparlma <dicksonparlma@gmail.com>
2019-04-27 10:49:07 +0300

    encapsulated the category class

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -1,3 +1,15 @@
 public class Category {
+    private String type;
+    private int id;
+    public Category(String Type){
+        this.type = Type;
+    }
 
+    public String gettype() {
+        return type;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
