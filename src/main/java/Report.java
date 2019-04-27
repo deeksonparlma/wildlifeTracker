0ea9540e91869fce6d8ebae4d208bf3eeb17c802
deeksonparlma <dicksonparlma@gmail.com>
2019-04-27 11:23:22 +0300

    encapsulate the class

--- src/main/java/Report.java
+++ src/main/java/Report.java
@@ -1,2 +1,16 @@
 public class Report {
+    private String rangerName;
+    private String category;
+    private String zone;
+    private String name;
+    private String health;
+    private String age;
+    public Report(String rangername,String category, String zone,String name,String health){
+      this.rangerName = rangername;
+      this.category = category;
+      this.zone = zone;
+      this.rangerName = rangername;
+      this.name = name;
+      this.health =health;
+    }
 }
