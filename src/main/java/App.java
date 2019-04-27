d7e171b24c3613f2b8e89be3af7695dfa9da4c90
deeksonparlma <dicksonparlma@gmail.com>
2019-04-27 11:51:10 +0300

    added a port manager

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,19 @@
+import static spark.Spark.port;
+import static spark.Spark.staticFileLocation;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        //port manager//
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+    }
 }
