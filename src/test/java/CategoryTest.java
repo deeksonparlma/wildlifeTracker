cf02c867500ed780c4fe99754250407c63269318
deeksonparlma <dicksonparlma@gmail.com>
2019-04-29 10:21:13 +0300

    added tests

---
+++ src/test/java/CategoryTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    @Test
+    public void getType() {
+    }
+
+    @Test
+    public void equals1() {
+    }
+
+    @Test
+    public void all() {
+    }
+
+    @Test
+    public void getReport() {
+    }
+
+    @Test
+    public void find() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void getId() {
+    }
+}
---
+++ src/test/java/ReportTest.java
@@ -0,0 +1,70 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReportTest {
+
+    @Test
+    public void getTime() {
+    }
+
+    @Test
+    public void getTimer() {
+    }
+
+    @Test
+    public void getCategory() {
+    }
+
+    @Test
+    public void getRangerName() {
+    }
+
+    @Test
+    public void getAge() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getHealth() {
+    }
+
+    @Test
+    public void getZone() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getCategoryId() {
+    }
+
+    @Test
+    public void getAnimalCount() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void sort() {
+    }
+
+    @Test
+    public void find() {
+    }
+
+    @Test
+    public void update() {
+    }
+}
