667316ab37a2b0eb46269e634642f7b07d3dc443
deeksonparlma <dicksonparlma@gmail.com>
2019-04-29 10:27:47 +0300

    changed the index file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -31,7 +31,7 @@ public class App {
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             model.put("categories", Category.all());
-            model.put("template", "public/templates/category-list.vtl");
+            model.put("template", "public/templates/index.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
@@ -44,7 +44,7 @@ public class App {
         get("/categories", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("categories", Category.all());
-            model.put("template", "public/templates/category-list.vtl");
+            model.put("template", "public/templates/index.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
